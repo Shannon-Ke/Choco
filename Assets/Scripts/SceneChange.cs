704da5435b52a1af21be68797ab7bd4dea3eadd0
Shannon Ke <shannonke@gatech.edu>
2018-05-29 21:01:12 -0400

    updating scripts and added shop and settings scenes

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+    public Button enterButton, settingsButton;
+	void Start () {
+        enterButton.onClick.AddListener(Enter);
+        settingsButton.onClick.AddListener(Settings);
+	}
+    void Enter() { SceneManager.LoadScene("Store"); }
+    void Settings() { SceneManager.LoadScene("Settings"); }
+}
