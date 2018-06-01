8490762828bd25a2cb01dc7c317eea48ab79acd9
Shannon Ke <shannonke@gatech.edu>
2018-05-31 20:45:05 -0400

    made gamecontroller and new scenes

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class GameControl : MonoBehaviour {
+    public static GameControl control;
+    public string username;
+    public int money;
+    void Awake()
+    {
+        if (control == null) {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        } else if (control != this) {
+            Destroy(gameObject);
+        }
+    }
+    private void OnDisable()
+    {
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+        PlayerData data = new PlayerData();
+        data.username = username;
+        data.money = money;
+        binaryFormatter.Serialize(file, data);
+        file.Close();
+    }
+    private void OnEnable()
+    {
+        if (File.Exists(Application.persistentDataPath + "playerInfo.dat"))
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+            PlayerData data = (PlayerData)binaryFormatter.Deserialize(file);
+            file.Close();
+
+            username = data.username;
+            money = data.money;
+        } else {
+            
+        }
+    }
+}
+
+class PlayerData {
+    public string username;
+    public int money;
+}
---
+++ Assets/Scripts/SceneChangeShop.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeShop : MonoBehaviour
+{
+    public Button IDButton, moneyButton, townButton, factoryButton, inventoryButton, settingsButton;
+    public CanvasGroup inventory, id;
+    void Start()
+    {
+        IDButton.onClick.AddListener(Id);
+        factoryButton.onClick.AddListener(Factory);
+        settingsButton.onClick.AddListener(Settings);
+        inventoryButton.onClick.AddListener(Inventory);
+        townButton.onClick.AddListener(Map);
+        Deactivate(inventory);
+        Deactivate(id);
+    }
+    void Factory() { SceneManager.LoadScene("Factory"); }
+    void Settings() { SceneManager.LoadScene("Settings"); }
+    void Map() { SceneManager.LoadScene("Town"); }
+    void Inventory() {
+        if (id.interactable) { Deactivate(id); }
+        if (!inventory.interactable) { Activate(inventory); }
+        else { Deactivate(inventory); }
+    }
+    void Id() {
+        if (inventory.interactable) { Deactivate(inventory); }
+        if (!id.interactable) { Activate(id); }
+        else { Deactivate(id); }
+    }
+    void Deactivate(CanvasGroup canvas) {
+        canvas.alpha = 0f;
+        canvas.interactable = false;
+        canvas.blocksRaycasts = false;
+    }
+    void Activate (CanvasGroup canvas) {
+        canvas.alpha = 1f;
+        canvas.interactable = true;
+        canvas.blocksRaycasts = true;
+    }
+
+}
